5288b5ce3bd3685efec08b30c9e46e18e6c729aa
Michael Daul <michael.daul@duke.edu>
2014-06-27 11:25:49 -0400

    initial version of module

---
+++ bento.tpl.php
@@ -0,0 +1,69 @@
+<?php
+
+settype($Ntt, "string");
+settype($results, "string");
+settype($ctype, "string"); 
+extract($_GET, EXTR_IF_EXISTS);
+
+if(isset($Ntt))
+{
+	$queryTerms = stripslashes($Ntt);
+}
+else $queryTerms = "Search for articles, books &amp; more";
+
+$pageSize=preg_replace("[^0-9]","",$results); // sanitizing input to digits only
+if($pageSize == "" || $pageSize > 20) $pageSize = 20; // max 20 from endeca, hard coded in application
+
+if(isset($ctype) && $ctype!="")
+$contentType = stripslashes($ctype); 
+else $contentType = NULL;
+
+?>
+
+
+	<div class="search-resources-tabs">
+		<div class="tab-content">
+			<div id="articles" class="tab-pane active">			
+				<form class="form-inline" action="bento">
+				<!-- Replace 'action' URL with variable! -->
+
+					<input id="Ntt" type="text" name="Ntt" value="<?php echo $queryTerms; ?>" class="" placeholder="Search articles, books, journals, &amp; our website"> <button type="submit" class="btn btn-primary bannerSearch"> <em class="icon-search icon-white");"> &nbsp; </em> </button>
+
+				</form>
+			
+			</div>
+		</div>
+	</div>
+
+
+	<div class="span-4">
+
+		<?php
+		
+		include("assets/includes/summon_articles.php"); 
+	
+		?>
+	
+	</div>
+	
+	
+	<div class="span-4">
+		
+		<?php include("assets/includes/endeca.php"); ?>
+	
+	</div>
+	
+	
+	<div class="span-4">
+		
+		<?php include("assets/includes/libguides.php"); ?>
+		
+		<br /><hr /><br />
+		
+		<?php include("assets/includes/summon_images.php"); ?>
+		
+		<br /><hr /><br />
+		
+		<?php include("assets/includes/google_cse.php"); ?>
+	
+	</div>
