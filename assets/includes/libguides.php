168817243c52e559021961df2ed1935ab62b6557
Michael Daul <michael.daul@duke.edu>
2014-07-01 15:41:17 -0400

    switching libguide searh to use google CSE

--- assets/includes/libguides.php
+++ assets/includes/libguides.php
@@ -1,28 +1,23 @@
-<?php
 
+<h2>Research Guides <a href="http://library.duke.edu/search-results?as_q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;">See All »</a></h2>
+<p class="smaller muted">For assistance researching unfamiliar topics</p>
 
-echo '<h2>Research Guides <a href="http://guides.library.duke.edu/search.php?iid=150&gid=0&c=0&search=' . $queryTerms . '" class="callbox" style="margin-left: 10px;">See All »</a></h2>
-		<p class="smaller muted">For assistance researching unfamiliar topics</p>
-		<div class="results-panel">';
+<div class="results-panel">
 
+<script>
+  (function() {
+    var cx = '012356957315223414689:eqn6ecttvlm';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:searchresults-only></gcse:searchresults-only>
 
-if($queryTerms != "") {
-
-	$theSearch = urlencode($queryTerms);
-
-	echo "<div id='api_search_guides_iid150'></div>";
-	echo "<script type='text/javascript' src='http://api.libguides.com/api_search.php?iid=150&type=guides&limit=3&more=false&sortby=relevance&context=object&format=js&search=" . $theSearch . "'> </script>";
-	
-	}
-
-
-echo '</div>';
-	
-
-
-?>
-
-
-
+</div>
 
 
