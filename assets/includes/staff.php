987c9b72c3c6b04c1a4b56c5c5e5d043f05b2a8a
Michael Daul <michael.daul@duke.edu>
2014-08-06 11:00:44 -0400

    removing duplicates for org searches

--- assets/includes/staff.php
+++ assets/includes/staff.php
@@ -23,29 +23,31 @@ if($queryTerms != "") {
 
 	<?php
 
-
+	
 	# set the active database to 'django'
 	db_set_active('django');
 
 	$query = db_select('directory_person', 'p')
 		->fields('p');        // all columns in the 'directory_person table
-	//$query->join('directory_orgunit_members', 'm', 'p.id = m.person_id');
-	//$query->join('directory_orgunit', 'o', 'o.id = m.orgunit_id');
-	//$query->addField('o', 'name', 'dept_name');
+	$query->join('directory_orgunit_members', 'm', 'p.id = m.person_id');
+	$query->join('directory_orgunit', 'o', 'o.id = m.orgunit_id');
+	$query->addField('o', 'name', 'dept_name');
+	
 	$query->condition(
 		db_or()	->condition('p.first_name', '%' . $theSearch . '%', 'LIKE')
 				->condition('p.last_name', '%' . $theSearch . '%', 'LIKE')
 				->condition('p.nickname', '%' . $theSearch . '%', 'LIKE')
 				->condition('p.display_name', '%' . $theSearch . '%', 'LIKE')
-				->condition('p.title', '%' . $theSearch . '%', 'LIKE')
+				->condition('p.preferred_title', '%' . $theSearch . '%', 'LIKE')
 				->condition('p.profile', '%' . $theSearch . '%', 'LIKE')
 				->condition('p.keywords', '%' . $theSearch . '%', 'LIKE')
+				->condition('o.name', '%' . $theSearch . '%', 'LIKE')
 		);
-	$query->distinct(); // make distinct
+	//$query->distinct(); // make distinct
 	//$query->orderBy('p.id', 'ASC');
 	$query->orderBy('p.last_name', 'ASC');
 	$query->orderBy('p.first_name', 'ASC');
-	$query->range(0,3); // limit to 3 results
+	$query->range(0,10); // limit to 10 results
 
 
 	$staff_persons = $query->execute();
@@ -58,61 +60,79 @@ if($queryTerms != "") {
 
 	} else {
 	
+		
+		$staffNamesList = array();
+		
+		$staffCount = 0;
+	
 	
 		foreach ($staff_persons as $person) {
 
-			// Set Profile
-			$theProfile = $person->profile;
+			if ((!in_array($person->display_name, $staffNamesList)) && ($staffCount < 3)) {
 				
-				// remove html characters
-				$theProfile = strip_tags(str_replace('&nbsp;',' ',$theProfile));
 				
-				// truncate long profiles
-				if (strlen($theProfile) > 150) {
-					$theProfile = wordwrap($theProfile, 150);
-					$theProfile = substr($theProfile, 0, strpos($theProfile, "\n"));
-					$theProfile = $theProfile . ' (&hellip;)';
-				}
+				// Set Profile
+				$theProfile = $person->profile;
+				
+					// remove html characters
+					$theProfile = strip_tags(str_replace('&nbsp;',' ',$theProfile));
+				
+					// truncate long profiles
+					if (strlen($theProfile) > 150) {
+						$theProfile = wordwrap($theProfile, 150);
+						$theProfile = substr($theProfile, 0, strpos($theProfile, "\n"));
+						$theProfile = $theProfile . ' (&hellip;)';
+					}
 			
 		
 		
-			// Set Title
-			if ($person->preferred_title != "") {
-				$theTitle = $person->preferred_title;
-			} else {
-				$theTitle = $person->title;
-			}
-				// truncate long title
-				if (strlen($theTitle) > 50) {
-					$theTitle = wordwrap($theTitle, 50);
-					$theTitle = substr($theTitle, 0, strpos($theTitle, "\n"));
-					$theTitle = $theTitle . ' (&hellip;)';
-				}
-
-
-
-			echo '<div class="personResult" title="' . $theProfile . '">';
-	
-	
-				if ($person->photo_url != "") {
-		
-					echo '<div class="thumbnail">';
-					echo '<a href="/about/directory/staff/' . $person->id .  '/"><img src="' . $person->photo_url . '"></a>';
-					echo '</div>';
-	
+				// Set Title
+				if ($person->preferred_title != "") {
+					$theTitle = $person->preferred_title;
 				} else {
-			
-					echo '<div class="thumbnail">';
-					echo '<a href="/about/directory/staff/' . $person->id .  '/"><img src="http://libcms.oit.duke.edu/sites/default/files/dul/directory/no_photo_reading_devil.png"></a>';
-					echo '</div>';
-
+					$theTitle = $person->title;
 				}
+					// truncate long title
+					if (strlen($theTitle) > 50) {
+						$theTitle = wordwrap($theTitle, 50);
+						$theTitle = substr($theTitle, 0, strpos($theTitle, "\n"));
+						$theTitle = $theTitle . ' (&hellip;)';
+					}
+
+
+
+				echo '<div class="personResult" title="' . $theProfile . '">';
 	
-				echo '<div class="name"><a href="/about/directory/staff/' . $person->id .  '/">' . $person->display_name . '</a></div>';
-				echo '<div class="title">' . $theTitle . '</div>';
+	
+					if ($person->photo_url != "") {
+		
+						echo '<div class="thumbnail">';
+						echo '<a href="/about/directory/staff/' . $person->id .  '/"><img src="' . $person->photo_url . '"></a>';
+						echo '</div>';
+	
+					} else {
+			
+						echo '<div class="thumbnail">';
+						echo '<a href="/about/directory/staff/' . $person->id .  '/"><img src="http://libcms.oit.duke.edu/sites/default/files/dul/directory/no_photo_reading_devil.png"></a>';
+						echo '</div>';
+
+					}
+	
+					echo '<div class="name"><a href="/about/directory/staff/' . $person->id .  '/">' . $person->display_name . '</a></div>';
+					echo '<div class="title">' . $theTitle . '</div>';
 	
 	
-			echo '</div>';
+				echo '</div>';
+				
+			
+			// add display name to array to compare	
+			array_push($staffNamesList, $person->display_name);
+			
+			// increment counter
+			$staffCount += 1;
+			
+				
+			}
 		
 		}
 
