6e3ef81c5382613507bbba8c2bec29d6923a98a1
Michael Daul <michael.daul@duke.edu>
2014-08-21 10:02:09 -0400

    updating see all links to point to drupal result page

--- assets/includes/google_libguides.php
+++ assets/includes/google_libguides.php
@@ -6,7 +6,7 @@ if($queryTerms != "") {
 
 	<div class="results-block first" id="results-libguides">
 
-		<h2>Research Guides <a href="http://www.google.com/cse?cx=012356957315223414689%3Aeqn6ecttvlm&ie=UTF-8&q=#gsc.tab=0&gsc.sort=&gsc.q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'ResearchGuides\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
+		<h2>Research Guides <a href="/research/guides/results?as_q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'ResearchGuides\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
 		<p class="smaller muted">For assistance researching this topic</p>
 	
 		<div class="results-panel">
--- assets/includes/google_website.php
+++ assets/includes/google_website.php
@@ -6,7 +6,7 @@ if($queryTerms != "") {
 
 	<div class="results-block" id="results-website">
 
-		<h2>Our Website <a href="http://www.google.com/cse?cx=012356957315223414689%3Atvt-hlpicwm&ie=UTF-8&q=#gsc.tab=0&gsc.sort=&gsc.q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'OurWebsite\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
+		<h2>Our Website <a href="/find/website?as_q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'OurWebsite\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
 		<p class="smaller muted">Collections, policies, news &amp; more</p>
 
 		<div class="results-panel">
