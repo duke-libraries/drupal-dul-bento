4fc1e65f5c92eb9c61fe842bf321e17e92950b7a
Michael Daul <michael.daul@duke.edu>
2014-08-21 10:30:06 -0400

    fixing google search variable

--- assets/includes/google_website.php
+++ assets/includes/google_website.php
@@ -6,7 +6,7 @@ if($queryTerms != "") {
 
 	<div class="results-block" id="results-website">
 
-		<h2>Our Website <a href="/find/website?as_q=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'OurWebsite\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
+		<h2>Our Website <a href="/find/website?Ntt=<?php echo $queryTerms; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onclick="_gaq.push([\'_trackEvent\', \'BentoResults\', \'OurWebsite\', \'SeeAll\']);"' ?>>See&nbsp;All&nbsp;&raquo;</a></h2>
 		<p class="smaller muted">Collections, policies, news &amp; more</p>
 
 		<div class="results-panel">
