1db9f76e41c9f7430755231ebee2fa1f7577461f
Cory Lown <cory.lown@duke.edu>
2015-04-13 14:46:50 -0400

    remove fade out of best bets flag

--- assets/js/combined.js
+++ assets/js/combined.js
@@ -1096,7 +1096,7 @@ jQuery(document).ready(function(){
 
 jQuery(document).ready(function(){
     if (window.innerWidth > 640) {
-        jQuery('.best-bet-flag').delay(50).fadeIn(500).delay(10000).fadeOut(500);
+        jQuery('.best-bet-flag').delay(50).fadeIn(500);
     } else {
         jQuery('.best-bet-flag').remove();
     }
