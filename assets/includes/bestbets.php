803bca5edd47a19838f42fb9858d7de42f4c242b
Cory Lown <cory.lown@duke.edu>
2015-04-20 15:39:38 -0400

    add logging of best bet serves and logging of clicks to ga

--- assets/includes/bestbets.php
+++ assets/includes/bestbets.php
@@ -2,6 +2,8 @@
 
 require_once('SolrPhpClient/Service.php');
 
+$logfile = 'private://bestbets_log.txt';
+
 //$queryTerms = 'thompson one'; // switch to use $queryTerms
 
 // Remove these special characters:  ( ) ? * [ ] ` ! # $ % { } < > ^ ~ | + & 
@@ -54,12 +56,20 @@ try {
     $errors[] = $e->getMessage();
 }
 
+$id = $results->response->docs[0]->id;
 $title = $results->response->docs[0]->title;
 $url = $results->response->docs[0]->url;
 $description = $results->response->docs[0]->description;
 
-if ($title && $url) {
-    echo '<h3><a href="' . $url . '" class="best-bet-link">' . $title . '</a>&nbsp;&nbsp;&nbsp;<span class="best-bet-flag">Best Bet</span></h3>';
+if ($title && $url && $id) {
+
+    $bestbet_loginfo = date('Y-m-d H:i:s');
+    $bestbet_loginfo .= "\tbb_serve";
+    $bestbet_loginfo .= "\t" . $id . "\n";
+
+    file_put_contents($logfile, $bestbet_loginfo, FILE_APPEND | LOCK_EX);
+
+    echo '<h3><a href="' . $url . '" class="best-bet-link" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'BestBets\', eventLabel: \'' . $title . '\'})">' . $title . '</a>&nbsp;&nbsp;&nbsp;<span class="best-bet-flag">Best Bet</span></h3>';
     echo '<p class="best-bet-description">' . $description . '</p>';
 }
 
