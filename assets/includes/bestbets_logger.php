5cff8ca34073468c3e2c8702bf6d7a4c15876e7b
Cory Lown <cory.lown@duke.edu>
2015-04-21 10:53:07 -0400

    add script to log best bet clicks

---
+++ assets/includes/bestbets_logger.php
@@ -0,0 +1,37 @@
+<?php
+
+$logfile = '/srv/web/libcms/backup/bestbets_log.txt';
+
+if (isset($_GET['event'])) {
+    $event = $_GET['event'];
+
+    if ($event !== 'bb_serve' and $event !== 'bb_click') {
+        $event = null;
+    }
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    if (strlen($id) > 30) {
+        $id = null;
+    }
+
+    if (!ctype_alpha($id)) {
+        $id = null;
+    }
+}
+
+if (isset($id) && isset($event)) {
+    $bestbet_loginfo = date('Y-m-d H:i:s');
+    $bestbet_loginfo .= "\t" . $event;
+    $bestbet_loginfo .= "\t" . $id . "\n";
+
+    //echo $bestbet_loginfo;
+
+    file_put_contents($logfile, $bestbet_loginfo, FILE_APPEND | LOCK_EX);
+}
+
+
+
+?>
