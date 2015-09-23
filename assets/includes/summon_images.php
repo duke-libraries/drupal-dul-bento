3f8e0e20dff2d38100c772c9311361934943be9d
mikedaul <michael.daul@gmail.com>
2015-09-23 09:54:58 -0400

    fixed IF for anchor link

--- assets/includes/summon_images.php
+++ assets/includes/summon_images.php
@@ -27,7 +27,7 @@ $theSearch = urlencode($queryTerms);
 if($queryTerms != "") {
 
 
-	echo '<div class="results-block" id="results-libguides">';
+	echo '<div class="results-block" id="results-images">';
 
 		echo '<h2>Images <a href="http://duke.summon.serialssolutions.com/advanced#!/search?ho=t&fvf=ContentType,Image,f|ContentType,Photograph,f&l=en&q=' . $queryDisplay . '" class="callbox" style="margin-left: 10px;" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'Images\', eventLabel: \'SeeAll\'});">See All »</a></h2>
 				<p class="smaller muted">Digitized collections</p>
