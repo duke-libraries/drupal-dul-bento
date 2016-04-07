8cd1ee7eb483edc55a19e7655a9ea018423ec750
Michael Daul <michael.daul@duke.edu>
2016-04-07 10:58:02 -0400

    updated 'view all' links

--- assets/includes/summon_articles.php
+++ assets/includes/summon_articles.php
@@ -32,7 +32,7 @@ if($queryTerms != "") {
 
 echo '<div class="results-block first" id="results-articles">';
 
-	echo '<h2>Articles <a href="http://duke.summon.serialssolutions.com/advanced#!/search?ho=t&fvf=ContentType,Journal%20Article,f|ContentType,Magazine%20Article,f&l=en&q=' . $queryDisplay . '" class="callbox" style="margin-left: 10px;" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'Articles\', eventLabel: \'SeeAll\'});">See&nbsp;All&nbsp;&raquo;</a></h2>
+	echo '<h2>Articles <a href="http://duke.summon.serialssolutions.com/advanced#!/search?ho=t&fvf=ContentType,Journal%20Article,f|ContentType,Magazine%20Article,f|ContentType,Conference%20Proceeding,f&l=en&q=' . $queryDisplay . '" class="callbox" style="margin-left: 10px;" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'Articles\', eventLabel: \'SeeAll\'});">See&nbsp;All&nbsp;&raquo;</a></h2>
 			<p class="smaller muted">From journals and magazines</p>
 			<div class="results-panel">';
 
@@ -306,7 +306,7 @@ echo '<div class="results-block first" id="results-articles">';
 
 		echo '<div class="see-all">';
 
-			echo '<a href="http://duke.summon.serialssolutions.com/advanced#!/search?ho=t&fvf=ContentType,Journal%20Article,f|ContentType,Magazine%20Article,f&l=en&q=' . $queryDisplay . '" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'Articles\', eventLabel: \'SeeAllBottom\'});">See All Results</a>';
+			echo '<a href="http://duke.summon.serialssolutions.com/advanced#!/search?ho=t&fvf=ContentType,Journal%20Article,f|ContentType,Magazine%20Article,f|ContentType,Conference%20Proceeding,f&l=en&q=' . $queryDisplay . '" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'Articles\', eventLabel: \'SeeAllBottom\'});">See All Results</a>';
 
 		echo '</div>';
 
