01937ade3d8ea33f8d3031fa2d681bec0bd38acd
mikedaul <michael.daul@duke.edu>
2020-09-04 09:40:02 -0400

    added '&group=true' to end of arclight links

--- assets/includes/google_collection_guides.php
+++ assets/includes/google_collection_guides.php
@@ -6,7 +6,7 @@ if($queryTerms != "") {
 
 	<div class="results-block first" id="results-collection-guides">
 
-		<h3><div class="anchor-highlight hide">»</div> Collection Guides <a href="//archives.lib.duke.edu/?q=<?php echo $queryDisplay; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'SeeAll\'});"' ?>>See&nbsp;All&nbsp;&raquo;</a></h3>
+		<h3><div class="anchor-highlight hide">»</div> Collection Guides <a href="//archives.lib.duke.edu/?q=<?php echo $queryDisplay; ?>&group=true" class="callbox" style="margin-left: 10px;" <?php echo 'onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'SeeAll\'});"' ?>>See&nbsp;All&nbsp;&raquo;</a></h3>
 		<p class="small text-muted">Detailed inventories of archival collections</p>
 
 		<div class="results-panel" onClick="ga('send', 'event', { eventCategory: 'BentoResults', eventAction: 'CollectionGuides', eventLabel: 'resultsDiv'})">
@@ -19,7 +19,7 @@ if($queryTerms != "") {
 
 		<div class="see-all">
 
-			<a href="//archives.lib.duke.edu/?q=<?php echo $queryDisplay; ?>" onclick="ga('send', 'event', { eventCategory: 'BentoResults', eventAction: 'CollectionGuides', eventLabel: 'SeeAllBottom'});">See all collection guide results</a>
+			<a href="//archives.lib.duke.edu/?q=<?php echo $queryDisplay; ?>&group=true" onclick="ga('send', 'event', { eventCategory: 'BentoResults', eventAction: 'CollectionGuides', eventLabel: 'SeeAllBottom'});">See all collection guide results</a>
 
 		</div>
 
