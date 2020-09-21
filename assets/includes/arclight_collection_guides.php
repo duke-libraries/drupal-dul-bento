502785cf53810d000a94fa1b1065e052c288d408
mikedaul <michael.daul@duke.edu>
2020-09-21 11:50:45 -0400

    adding arclight icons

--- assets/includes/arclight_collection_guides.php
+++ assets/includes/arclight_collection_guides.php
@@ -107,7 +107,7 @@ if ($theSearch != "") {
 
 		<div class="resultsHeader">
 
-				<h3><div class="anchor-highlight hide">»</div> Collection Guides <a href="<?php echo $searchURLAll . $theSearch . $allCampaignParams; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'SeeAll\'});"' ?>>See&nbsp;All&nbsp;&raquo;</a></h3>
+				<h3><div class="anchor-highlight hide">»</div> Collection Guides <a href="<?php echo $searchURLGrouped . $theSearch . $allCampaignParams; ?>" class="callbox" style="margin-left: 10px;" <?php echo 'onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'SeeAll\'});"' ?>>See&nbsp;All&nbsp;&raquo;</a></h3>
 
 				<p class="small text-muted">Detailed inventories of archival collections</p>
 
@@ -136,6 +136,12 @@ if ($theSearch != "") {
         $parentLabels = $theJSON['data'][$i]['attributes']['parent_labels']['attributes']['value'];
         $parentLabelsLabel = $theJSON['data'][$i]['attributes']['parent_labels']['attributes']['label'];
         $parentIDs = $theJSON['data'][$i]['attributes']['parent_ids']['attributes']['value'];
+        $onlineComponent = $theJSON['data'][$i]['attributes']['online_content?']['attributes']['value'];
+        $restrictedComponent = $theJSON['data'][$i]['attributes']['restricted_component?']['attributes']['value'];
+
+        $onlineIconBlock = '<span data-toggle="tooltip" title="" data-original-title="Available Online"><span class="blacklight-icons al-online-content-icon"><!--?xml version="1.0"?--><svg viewBox="0 0 640 512" role="img" aria-label="online icon"><path d="M608 0H160a32 32 0 0 0-32 32v96h160V64h192v320h128a32 32 0 0 0 32-32V32a32 32 0 0 0-32-32zM232 103a9 9 0 0 1-9 9h-30a9 9 0 0 1-9-9V73a9 9 0 0 1 9-9h30a9 9 0 0 1 9 9zm352 208a9 9 0 0 1-9 9h-30a9 9 0 0 1-9-9v-30a9 9 0 0 1 9-9h30a9 9 0 0 1 9 9zm0-104a9 9 0 0 1-9 9h-30a9 9 0 0 1-9-9v-30a9 9 0 0 1 9-9h30a9 9 0 0 1 9 9zm0-104a9 9 0 0 1-9 9h-30a9 9 0 0 1-9-9V73a9 9 0 0 1 9-9h30a9 9 0 0 1 9 9zm-168 57H32a32 32 0 0 0-32 32v288a32 32 0 0 0 32 32h384a32 32 0 0 0 32-32V192a32 32 0 0 0-32-32zM96 224a32 32 0 1 1-32 32 32 32 0 0 1 32-32zm288 224H64v-32l64-64 32 32 128-128 96 96z"></path></svg></span></span>';
+
+        $restrictedIconBlock = '<span data-toggle="tooltip" title="" data-original-title="Some restrictions apply"><span class="blacklight-icons al-restricted-component-icon"><!--?xml version="1.0"?--><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" class="svg-inline--fa fa-exclamation-triangle fa-w-18" role="img" viewBox="0 0 576 512" aria-label="exclamation_triangle_solid icon"><path fill="currentColor" d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path></svg></span></span>';
 
 
 				// Title
@@ -202,6 +208,22 @@ if ($theSearch != "") {
 	      }
 
 
+        // icons
+        if ($onlineComponent == 'true' || $restrictedComponent == 'true') {
+          $iconRender = '<div class="icon-wrapper">';
+        }
+        
+        if ($onlineComponent == 'true') {
+          $iconRender .= $onlineIconBlock;
+        }
+
+        if ($restrictedComponent == 'true') {
+          $iconRender .= $restrictedIconBlock;
+        }
+
+        if ($onlineComponent == 'true' || $restrictedComponent == 'true') {
+          $iconRender .= '</div>';
+        }
 
 
 
@@ -210,7 +232,7 @@ if ($theSearch != "") {
 					echo '<div class="title">';
 						echo '<div class="text">';
 
-							echo '<h3 class="resultTitle"><a href="' . $baseURL . 'catalog/' . $theID . $itemCampaignParams . '" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'ItemTitle' . $resultCount . '\'});">' . $theTitle . '</a></h3>';
+							echo '<h3 class="resultTitle"><a href="' . $baseURL . 'catalog/' . $theID . $itemCampaignParams . '" onClick="ga(\'send\', \'event\', { eventCategory: \'BentoResults\', eventAction: \'CollectionGuides\', eventLabel: \'ItemTitle' . $resultCount . '\'});">' . $theTitle . '</a>' . $iconRender . '</h3>';
 
 						echo '</div>';
 					echo '</div>';
@@ -279,6 +301,8 @@ if ($theSearch != "") {
         unset($parentLabels);
         unset($parentLabelsLabel);
         unset($parentIDs);
+        unset($onlineComponent);
+        unset($restrictedComponent);
 
 				unset($theTitle);
 				unset($theID);
